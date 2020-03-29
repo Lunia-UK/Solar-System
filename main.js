36799bc8a2f1cb795d01d6930e0a368a66b20a0d
Lunia-UK <57706009+Lunia-UK@users.noreply.github.com>
2020-03-29 22:31:52 +0200

    Add Mercury and Switch

--- main.js
+++ main.js
@@ -5,21 +5,43 @@ let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+
+// Mercury 
+const textureMercury = new THREE.TextureLoader().load('texture/mercury/mercurymap.jpg');
+let geometryMercury = new THREE.SphereGeometry( 13, 32, 32 );
+const materialMercury = new THREE.MeshBasicMaterial({ map: textureMercury });
+let mercuryMesh = new THREE.Mesh( geometryMercury, materialMercury );
+materialMercury.bumpMap = THREE.ImageUtils.loadTexture('texture/mercury/mercurybump.jpg')
+materialMercury.bumpScale = 0.05
+scene.add( mercuryMesh );
+
 // Moon 
-const texture = new THREE.TextureLoader().load('texture/moon/moonmap2k.jpg');
-let geometry = new THREE.SphereGeometry( 13, 32, 32 );
-const material = new THREE.MeshBasicMaterial({ map: texture });
-let moonMesh = new THREE.Mesh( geometry, material );
-material.bumpMap = THREE.ImageUtils.loadTexture('texture/moon/moonbump2k.jpg')
-material.bumpScale = 0.05
-scene.add( moonMesh );
+const textureMoon = new THREE.TextureLoader().load('texture/moon/moonmap2k.jpg');
+let geometryMoon = new THREE.SphereGeometry( 13, 32, 32 );
+const materialMoon = new THREE.MeshBasicMaterial({ map: textureMoon });
+let moonMesh = new THREE.Mesh( geometryMoon, materialMoon );
+materialMoon.bumpMap = THREE.ImageUtils.loadTexture('texture/moon/moonbump2k.jpg')
+materialMoon.bumpScale = 0.05
+
 
 camera.position.z = 50;
+
+const btnMoon = document.querySelector('#moon')
+const btnMercury = document.querySelector('#mercury')
+btnMoon.addEventListener('click', function displayMoon(){
+    scene.add( moonMesh )
+    scene.remove( mercuryMesh )
+})
+btnMercury.addEventListener('click', function displayMercury(){
+    scene.add( mercuryMesh )
+    scene.remove( moonMesh )
+})
 
 let animate = function () {
     requestAnimationFrame( animate );
 
     moonMesh.rotation.y += 0.0025;
+    mercuryMesh.rotation.y += 0.0025;
 
     renderer.render( scene, camera );
 };
